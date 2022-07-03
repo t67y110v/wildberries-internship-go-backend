5e85770b749dd4ed16d625753fd56493eb954a5e
Vladimir Nesterov <46971653+t67y110v@users.noreply.github.com>
2022-07-03 22:05:21 +0400

    cnfg

--- config/config.go
+++ config/config.go
@@ -4,8 +4,8 @@ import "os"
 
 func ConfigurationSetUp() {
 	//конфиги бд
-	os.Setenv("DB_USERNAME", "p02tgre2")
-	os.Setenv("DB_PASSWORD", "p02tgre2")
+	os.Setenv("DB_USERNAME", "DB_USERNAME")
+	os.Setenv("DB_PASSWORD", "DB_PASSWORD")
 	os.Setenv("DB_HOST", "5432")
 	os.Setenv("DB_NAME", "Orders")
 	os.Setenv("DB_POOL_MAXCONN", "5")
